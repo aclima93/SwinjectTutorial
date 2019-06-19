def72aed38061617b0566f71d39765aa0f67b971
António Lima <tocalagarto93@gmail.com>
2019-06-19 18:00:40 +0100

    refactoring DI for BitcoinPriceFetcher instance in BitcoinVC - this addresses dependencies that are tied to storyboards

--- Bitcoin Adventurer/Dependencies/SwinjectStoryboard+Extension.swift
+++ Bitcoin Adventurer/Dependencies/SwinjectStoryboard+Extension.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2019 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,58 +26,22 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
+import Foundation
+import Swinject
+import SwinjectStoryboard
+import SwinjectAutoregistration
 
-internal class BitcoinViewController: UIViewController {
+extension SwinjectStoryboard {
   
-  @IBOutlet weak private var checkAgain: UIButton!
-  @IBOutlet weak private var primary: UILabel!
-  @IBOutlet weak private var partial: UILabel!
-  
-  var fetcher: PriceFetcher?
-  
-  private let dollarsDisplayFormatter: NumberFormatter = {
-    let formatter = NumberFormatter()
-    formatter.maximumFractionDigits = 0
-    formatter.numberStyle = .currency
-    formatter.currencySymbol = ""
-    formatter.currencyGroupingSeparator = ","
-    return formatter
-  }()
-  
-  private let standardFormatter = NumberFormatter()
-  
-  // MARK: - View lifecycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    requestPrice()
-  }
-
-  // MARK: - Actions
-  @IBAction private func checkAgainTapped(sender: UIButton) {
-    requestPrice()
-  }
-  
-  // MARK: - Private methods
-  private func updateLabel(price: Price) {
-    guard let dollars = price.components().dollars,
-          let cents = price.components().cents,
-          let dollarAmount = standardFormatter.number(from: dollars) else { return }
+  @objc class func setup() {
+    // Every time a Networking protocol is requested, a resolved instance of HTTPNetworking is returned.
+    // The same goes for the PriceFetcher protocol and a resolved instance of BitcoinPriceFetcher.
+    defaultContainer.autoregister(Networking.self, initializer: HTTPNetworking.init)
+    defaultContainer.autoregister(PriceFetcher.self, initializer: BitcoinPriceFetcher.init)
     
-    primary.text = dollarsDisplayFormatter.string(from: dollarAmount)
-    partial.text = ".\(cents)"
-  }
-  
-  private func requestPrice()  {
-    guard let fetcher = fetcher else { fatalError("Missing dependencies") }
-    
-    fetcher.fetch { response in
-      guard let response = response else { return }
-
-      // 3. Update the UI with the parsed PriceResponse
-      DispatchQueue.main.async { [weak self] in
-        self?.updateLabel(price: response.data)
-      }
+    // resolves the registered dependency in the VC
+    defaultContainer.storyboardInitCompleted(BitcoinViewController.self) { resolver, controller in
+      controller.fetcher = resolver ~> PriceFetcher.self
     }
   }
 }
